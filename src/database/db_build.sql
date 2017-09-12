8db9427eb6b8943e8c192ee6d8f341c97ccc28f7
katiaash <katia.ashkar7@gmail.com>
2017-09-12 15:41:59 +0300

    fix the db_build.sql values relates #31

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -11,21 +11,23 @@ CREATE TABLE users (
 CREATE TABLE addActivityTime (
   id SERIAL PRIMARY KEY,
   user_id TEXT NOT NULL,
-  day TEXT NOT NULL,
-  work VARCHAR,
-  sleep VARCHAR,
-  eat VARCHAR,
-  fitness VARCHAR,
-  study VARCHAR,
-  school VARCHAR,
-  entertainment VARCHAR,
-  errand VARCHAR
+  activity TEXT NOT NULL,
+  today_date DATE,
+  activity_time TIME
 );
 
 INSERT INTO users (name, password) VALUES
-('Katia', 'katia123'), ('Heather', 'heather213');
+('Katia', 'katia123'),
+('Heather', 'heather213'),
+('Mario', 'MarioAlBatata'),
+('Harry', 'Harry1Potter');
 
-INSERT INTO addActivityTime(user_id, day, work, sleep, eat, fitness, study, school, entertainment, errand) VALUES
-('1', 'Monday', '4:00', '6:00', '00:15', '00:20', '0:50', '4:00', '00:25', '01:25'), ('2', 'Friday', '2:00', '6:00', '00:15', '00:20', '1:00', '6:00', '00:10', '00:25');
+INSERT INTO addActivityTime(user_id, activity, today_date, activity_time) VALUES
+('1', 'sleep', '11/7/2017', '06:00'),
+('2', 'eat', '11/7/2017', '00:15'),
+('3', 'fitness', '12/7/2017', '1:30'),
+('4', 'study', '20/7/2017', '05:10'),
+('2', 'eat', '18/7/2017', '00:10'),
+('3', 'sleep', '11/7/2017', '08:00');
 
 COMMIT;
