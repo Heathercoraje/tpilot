416a3effbd9b28dc35047a6d4aab738bdf92caa4
heathercoraje <yhk7830@hotmail.com>
2017-09-14 15:53:31 +0300

    make request relates #40

--- public/main.js
+++ public/main.js
@@ -12,32 +12,3 @@
 //   xhr.send();
 // }
 //
-// function update(err, data) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     var totalData = JSON.parse(data);
-//     /*have to send this data to html tag */
-//     var totaltime = document.querySelector('#totalTime');
-//   }
-// };
-// console.log('This is Tilot front-end main.js');
-//
-// var activity = document.querySelector('#activity');
-//
-// activity.addEventListener('click', function(event) {
-//   // when click any of those button, it leads to a // new page
-//   makeRequest('GET', '/help', function(error, response) {
-//     if (error) {
-//       throw error;
-//     } else
-//   });
-// });
-//
-//
-//
-// // var stopButton = document.querySelector('#stopButton');
-// //
-// // stopButton.addEventListener('click', function(event) { // go back to base page and update total time
-// //   request('/', update);
-// // });
