dd4a5d5e69b5a9c407ba3de9ceed8da7376954fc
heathercoraje <yhk7830@hotmail.com>
2017-09-17 14:15:44 +0300

    render a popup but it needs to be a modal instead of hbs file

--- src/controllers/popup.js
+++ src/controllers/popup.js
@@ -1,1 +1,14 @@
-console.log('hello');
+console.log('hello,this is popup.js');
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+
+
+router.get('/popup', (request, response) => {
+  response.render('popup');
+
+});
+
+
+module.exports = router;
