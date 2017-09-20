99de0eeec9bc599b11e139244dad38e2cc079bd5
heathercoraje <yhk7830@hotmail.com>
2017-09-20 10:38:05 +0300

    activePage bug fix

--- src/controllers/insight.js
+++ src/controllers/insight.js
@@ -1,7 +1,13 @@
 module.exports = (request, response) => {
+
+
+  //bring query to get data from database
+  //do calculation
+
   response.render('insight', {
     activePage: {
       insight: true
-    }
+    },
+    //  data
   });
 };
